6f1acc0cbc764134de60138d4fac7b9a47c52b1c
umarkhan <ku6205939@gmail.com>
2024-05-14 00:29:27 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,18 @@
+// task 1
+/*********
+ * ENUMS *
+ *********/
+
+enum Days {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+  }
+  
+  const today: Days = Days.Wednesday;
+  
+  console.log(`Today is ${Days[today]}`);
